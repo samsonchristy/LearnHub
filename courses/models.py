385f55136b080fc72f1b8706373dff05cbea0a49
samson christy <samsonchristy1990@gmail.com>
2020-06-11 11:07:16 +0530

    Added modules to a course, Added different types of content to each module and reorder contents

--- courses/models.py
+++ courses/models.py
@@ -70,7 +70,7 @@ class File(ItemBase):
     file = models.FileField(upload_to='files')
 
 class Image(ItemBase):
-    file = models.ImageField(upload_to='images')
+    file = models.FileField(upload_to='images')
 
 class Video(ItemBase):
     url = models.URLField()
