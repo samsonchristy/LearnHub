1d7f9fb28a79d2689969bd9c5f05aa1a61f21e6d
samson christy <samsonchristy1990@gmail.com>
2020-06-15 08:48:47 +0530

    student registration system and student enrollment in courses integrated

--- courses/views.py
+++ courses/views.py
@@ -11,7 +11,7 @@ from django.apps import apps
 from django.forms.models import modelform_factory
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.db.models import Count
-
+from students.forms import CourseEnrollForm
 
 class OwnerMixin(object):#mixin
     def get_queryset(self):
@@ -159,3 +159,8 @@ class CourseListView(TemplateResponseMixin,View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView,self).get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course':self.object})
+        return context
